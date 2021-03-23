cf07b069fb71b83493cf8d13bd9b6c8b5791c2af
현상진 <78408946+HYUNSANGJIN@users.noreply.github.com>
2021-03-23 20:42:46 +0900

    Add files via upload

---
+++ Team1_BootStrap/js/team1.js
@@ -0,0 +1,29 @@
+function click_phone_icon() {
+  let timerInterval
+  Swal.fire({
+    title: "010-5775-2732",
+    html: "빨리 받아적으세요<br> <b></b>초남음 ",
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading()
+      timerInterval = setInterval(() => {
+        const content = Swal.getContent()
+        if (content) {
+          const b = content.querySelector('b')
+          if (b) {
+            b.textContent = Swal.getTimerLeft()
+          }
+        }
+      }, 100)
+    },
+    willClose: () => {
+      clearInterval(timerInterval)
+    }
+  }).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log('I was closed by the timer')
+    }
+  })
+}
